f2395756fa142186fa5fe1a5e615cee75619df32
NicolasCharpignon <nico.charpignon@orange.fr>
2024-12-09 11:27:11 +0100

    maj

--- gts_utilisateurs.sh
+++ gts_utilisateurs.sh
@@ -3,29 +3,64 @@
 # Fonction qui permet de créer un utilisateur
 
 
-create_user()
+read -p "Quelle action effectuer ? (create_user, delete_user, create_group) : " action
+read -p "Quel est le nom de l'utilisateur ? : " USER
 
-{
-    echo "Création de l'utilisateur : $1"
+#generation d'un mdp akeatoire  pour l'utilisateur
 
 
-    # On vérifie si l'utilisateur existe
+create_user() {
+    # Vérification si l'utilisateur existe déjà
     if id "$1" &>/dev/null; then
         echo "L'utilisateur $1 existe déjà"
     else
-        # On crée l'utilisateur
+        # Création de l'utilisateur
         sudo useradd -m -s /bin/bash "$1"
         echo "L'utilisateur $1 a été créé"
-        # On définit le mot de passe de l'utilisateur
-        sudo passwd 
 
+        # Génération du mot de passe aléatoire
+        password=$(openssl rand -base64 6)
+        echo "Mot de passe généré : $password"
+
+        # Affectation du mot de passe à l'utilisateur
+        echo "$1:$password" | sudo chpasswd
+
+        # Forcer l'utilisateur à changer son mot de passe au prochain login
+        sudo passwd -e "$1"
+
+        echo "L'utilisateur $1 a maintenant un mot de passe aléatoire."
+
+        # Affecter l'utilisateur à un ou plusieurs groupes
+        read -p "Souhaitez-vous affecter l'utilisateur $1 à un ou plusieurs groupes ? (y/n) : " response
+        if [ "$response" == "y" ]; then
+            # Lister les groupes existants
+            echo "Groupes existants :"
+            liste_groupes=$(cut -d: -f1 /etc/group | tr '\n' ' ')
+            echo "$liste_groupes"
+            
+            # Demander les groupes
+            read -p "Entrez les groupes séparés par un espace : " groupes
+
+            # Ajouter l'utilisateur aux groupes avec gpasswd
+            for group in $groupes; do
+                sudo gpasswd -a "$1" "$group"
+                #echo "L'utilisateur $1 a été ajouté au groupe $group"
+            done
+        fi
     fi
 }
+
 
 
 delete_user()
 
 {
+
+    #for group in $(groups $1 | cut -d: -f2); do
+    #   sudo gpasswd -d "$1" "$group"
+    #  echo "L'utilisateur $1 a été supprimé du groupe $group"
+    #done
+
     echo "Suppression de l'utilisateur : $1" 
     # On vérifie si l'utilisateur existe
     if id "$1" &>/dev/null; then
@@ -38,3 +73,32 @@ delete_user()
 
 }
 
+#creation d'un groupe
+create_group()
+{
+    echo "Création du groupe : $1"
+    # On vérifie si le groupe existe
+    if grep -q "^$1:" /etc/group; then
+        echo "Le groupe $1 existe déjà"
+    else
+        # On crée le groupe
+        sudo groupadd "$1"
+        echo "Le groupe $1 a été créé"
+    fi
+}
+
+
+case $action in
+    create_user)
+        create_user "$USER"
+        ;;
+    delete_user)
+        delete_user "$USER"
+        ;;
+    create_group)
+        create_group "$GROUP"
+        ;;
+    *)
+        echo "Action inconnue"
+        ;;
+esac
