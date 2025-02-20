808bb75f03e45a5585116556391b208f01380010
NicolasCharpignon <nico.charpignon@orange.fr>
2025-02-20 14:18:15 +0100

    chemin absolut

--- gts_script.sh
+++ gts_script.sh
@@ -4,7 +4,7 @@
 # Exécuter la préparation du système au démarrage, sans affichage dans le menu
 if [ ! -f "/tmp/system_prepared" ]; then
     echo -e "Préparation initiale du système en cours..."
-    chmod +x prepare_system.sh && prepare_system.sh
+    chmod +x /usr/local/bin/prepare_system.sh && /usr/local/bin/prepare_system.sh
     touch /tmp/system_prepared  # Marqueur pour éviter l'exécution multiple
     echo -e "Système préparé avec succès."
     sleep 2  # Petite pause avant d'afficher le menu
@@ -41,15 +41,15 @@ while true; do
     read -p "Choisissez une option (1-6) : " choice
 
     case $choice in
-        1) execute_script "gts_utilisateurs.sh"
+        1) execute_script "/usr/local/bin/gts_utilisateurs.sh"
            ;;
-        2) execute_script "gts_cron.sh"
+        2) execute_script "/usr/local/bingts_cron.sh"
            ;;
-        3) execute_script "gts_surveillance.sh"
+        3) execute_script "/usr/local/bin/gts_surveillance.sh"
            ;;
-        4) execute_script "gts_sauvegarde.sh"
+        4) execute_script "/usr/local/bin/gts_sauvegarde.sh"
            ;;
-        5) execute_script "gts_journalisation.sh"
+        5) execute_script "/usr/local/bin/gts_journalisation.sh"
            ;;
         6) echo -e " Quitter..."
            exit 0
