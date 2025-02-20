a1c9cf4b74d56d7310f1c48075aeb74f616a38a2
NicolasCharpignon <nico.charpignon@orange.fr>
2025-02-20 13:43:46 +0100

    revu code user

--- gts_utilisateurs.sh
+++ gts_utilisateurs.sh
@@ -2,73 +2,60 @@
 
 
 # Fonction qui permet de créer un utilisateur
-
 create_user() {
 
-    #Demander a l'utilisateur le nom de l'utilisateur
     read -p "Quel est le nom de l'utilisateur à créer ? : " username
 
-    # Vérification si l'utilisateur existe déjà
     if id "$username" &>/dev/null; then
         echo "L'utilisateur $1 existe déjà"
     else
-        # Création de l'utilisateur
+
         sudo useradd -m -s /bin/bash "$username"
         echo "L'utilisateur $1 a été créé"
 
-        # Génération du mot de passe aléatoire
         password=$(openssl rand -base64 6)
         echo "Mot de passe généré : $password"
 
-        # Affectation du mot de passe à l'utilisateur
         echo "$username:$password" | sudo chpasswd
 
-        # Forcer l'utilisateur à changer son mot de passe au prochain login
         sudo passwd -e --quiet "$username"
 
         echo "L'utilisateur $1 a maintenant un mot de passe aléatoire."
 
 
-        # Demander à l'utilisateur quel quota en mégaoctets il souhaite définir
-        echo "Quel quota en mégaoctets voulez-vous attribuer à l'utilisateur $username ?"
-        read quota_mb
+        read -p "Quel quota en mégaoctets voulez-vous attribuer à l'utilisateur $username ?" quota_mb
 
-        # Convertir le quota de Mo en Ko (1 Mo = 1024 Ko)
         quota_ko=$((quota_mb * 1024))
 
-        # Appliquer le quota à l'utilisateur sur la partition /home
         sudo setquota -u $1 $quota_ko 0 0 0 /home
 
-        # Afficher un message de confirmation
         echo "Le quota de $quota_mb Mo a été défini pour l'utilisateur $1 sur la partition /home."
 
 
-        # Affecter l'utilisateur à un ou plusieurs groupes
-            # Lister les groupes existants
-            echo "Groupes existants :"
-            liste_groupes=$(getent group | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}' | grep -vxF -f <(getent passwd | awk -F: '$3 >= 1000 {print $1}'))
-            echo "$liste_groupes"
+        # Affecter l'utilisateur à un groupe
+        echo "Groupes existants :"
+        # Récupérer la liste des groupes système avec un ID entre 1000 et 60000, en excluant les groupes qui correspondent à des utilisateurs système ayant un ID >= 1000.
+        liste_groupes=$(getent group | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}' | grep -vxF -f <(getent passwd | awk -F: '$3 >= 1000 {print $1}'))
+        echo "$liste_groupes"
             
-            # Demander le groupe
-            read -p "Entrez le groupes dans le quel vous voulez afecter $username : " groupe
+        read -p "Entrez le groupes dans le quel vous voulez afecter $username : " groupe
 
-            # Ajouter l'utilisateur au groupe
-            usermod -aG $groupe $username
+        usermod -aG $groupe $username
 
-            echo "L'utilisateur $username a été ajouté au groupe $groupe."
+        echo "L'utilisateur $username a été ajouté au groupe $groupe."
     fi
 }
 
+# Fonction qui permet de supprimer un utilisateur
+
 delete_user() {
-    #lister tt les users
+
     echo "Liste des utilisateurs :";
     liste_users=$(getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}')
     echo "$liste_users"
 
-    # Demander le nom de l'utilisateur à supprimer
     read -p "Quel utilisateur voulez-vous supprimer ? : " username
 
-    # Vérifier si l'utilisateur existe
     if ! id "$username" &>/dev/null; then
         echo "L'utilisateur $username n'existe pas."
         return 1
@@ -76,26 +63,21 @@ delete_user() {
 
     echo "Suppression de l'utilisateur : $username"
 
-    # Supprimer les quotas de l'utilisateur
     echo "Suppression des quotas..."
     setquota -u "$username" 0 0 0 0 /home 2>/dev/null
 
-    # Supprimer l'utilisateur de tous les groupes
     echo "Suppression de l'utilisateur des groupes secondaires..."
     user_groups=$(id -nG "$username" | tr ' ' '\n' | grep -v "^$username$")  
     for group in $user_groups; do
         sudo gpasswd -d "$username" "$group" 2>/dev/null
     done
 
-    # Supprimer ses tâches cron s'il en a
     echo "Suppression des tâches cron..."
     crontab -r -u "$username" 2>/dev/null
 
-    # Supprimer l'utilisateur (sans supprimer ses fichiers)
     echo "Suppression de l'utilisateur..."
     userdel -f "$username"
 
-    # Supprimer son répertoire personnel
     echo "Suppression du répertoire personnel de $username..."
     rm -rf "/home/$username"
 
@@ -104,21 +86,22 @@ delete_user() {
 
 
 #creation d'un groupe
+
 create_group()
 {
-    #demander le nom du groupe
+
     read -p "Entrez le nom du groupe à créer : " group_name
 
     echo "Création du groupe : $group_name"
-    # On vérifie si le groupe existe
+
     if grep -q "^$group_name:" /etc/group; then
         echo "Le groupe $group_name existe déjà"
     else
-        # On crée le groupe
+        
         sudo groupadd "$group_name"
         echo "Le groupe $group_name a été créé"
 
-        # On crée le dossier partagé du département 
+        
         mkdir -p /ShareFolders/$group_name
         chown root:root /ShareFolders
         chown :$1 /ShareFolders/$groupe_name
@@ -130,7 +113,6 @@ create_group()
 
 add_user_to_group()
 {
-# Boucle pour vérifier si l'utilisateur existe et si son GID est supérieur ou égal à 1000 et lister les user avant de demander le nom de l'utilisateur
     
     while true; do
         echo "Liste des utilisateurs :";
@@ -145,33 +127,28 @@ add_user_to_group()
         fi
     done
 
-    # Lister les groupes existants
     echo "Groupes existants :"
     liste_groupes=$(getent group | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}' | grep -vxF -f <(getent passwd | awk -F: '$3 >= 1000 {print $1}'))
     echo "$liste_groupes"
 
-    # Demander les groupes
     read -p "Entrez le groupe  : " groupe
 
-    # Ajouter l'utilisateur au groupe
     usermod -aG $groupe $user
 
     echo "L'utilisateur $user a été ajouté au groupe $groupe."
-
-
 }
+
+#liste de tous les utilisateurs
 
 list_all()
 {
-    # Récupérer la liste des utilisateurs avec un GID supérieur ou égal à 1000
+    
     users=$(getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 {print $1}')
 
-    # Parcourir chaque utilisateur
     for user in $users; do
-        # Récupérer les groupes de l'utilisateur sauf le group user en question
+        
         groups=$(id -nG "$user" | tr ' ' '\n' | grep -v "^$user$")
 
-        # Afficher les informations formatées
         echo "Utilisateur: $user"
         echo "Groupe: $groups"
         echo "---------------------"
